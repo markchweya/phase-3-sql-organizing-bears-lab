6190102f95bb295605abb89ee97937cad686ae2c
Mark <mark.nyambane@student.moringaschool.com>
2023-05-25 09:34:43 +0300

    Completed Assignment

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,11 +1,71 @@
-INSERT INTO
-bears(name, age, sex, color, temperament, alive)
-VALUES
-('Mr. Chocolate', 21, 'M','brown', 'angry', 1),
-('Rowdy', 8, 'M','black', 'calm', 0),
-('Tabby', 14, 'F','white', 'mid', 0),
-('Sergeant Brown', 9, 'F','white', 'calm', 1),
-('Melissa', 21, 'M','brown', 'angry', 1),
-('Grinch', 3, 'F','brown', 'angry', 0),
-('Wendy', 12, 'M','black', 'mid', 1),
-(NULL, 24, 'F','white', 'calm', 0)
+INSERT INTO BEARS(
+    NAME,
+    AGE,
+    SEX,
+    COLOR,
+    TEMPERAMENT,
+    ALIVE
+) VALUES (
+    'Mr. Chocolate',
+    21,
+    'M',
+    'brown',
+    'angry',
+    1
+),
+(
+    'Rowdy',
+    8,
+    'M',
+    'black',
+    'calm',
+    0
+),
+(
+    'Tabby',
+    14,
+    'F',
+    'white',
+    'mid',
+    0
+),
+(
+    'Sergeant Brown',
+    9,
+    'F',
+    'white',
+    'calm',
+    1
+),
+(
+    'Melissa',
+    21,
+    'M',
+    'brown',
+    'angry',
+    1
+),
+(
+    'Grinch',
+    3,
+    'F',
+    'brown',
+    'angry',
+    0
+),
+(
+    'Wendy',
+    12,
+    'M',
+    'black',
+    'mid',
+    1
+),
+(
+    NULL,
+    24,
+    'F',
+    'white',
+    'calm',
+    0
+)
